c7dbd9decf3fb2809ca6b78db5582151368707ce
JeanMoneger <jean.moneger@univ-poitiers.fr>
2021-07-31 14:03:39 +0200

    Replaced Quest Answ RT < 1.5 with NA (instead of deleting)

--- cleaning prolific.R
+++ cleaning prolific.R
@@ -69,13 +69,11 @@ dataRT<- subset(data, data$key_resp_7.corr!=0 )
 
 #### ALTERNATIVELY, IT MIGHT BE SMARTER TO REPLACE THOSE TRIALS WITH NA IN ORDER TO KEEP ORDER IN THE COMING STEPS, CONSIDER USING/
  data2 <-data
- data2$slider_2.rt[] <- lapply(data2, function(x) ifelse(data2$slider_2.rt <1.5, NA, data2$slider_2.rt))
-# nope !!! Does shiiiiiiiiit.
+ data2[, "slider_2.rt"][data2[, "slider_2.rt"] <1.5] <- NA
  
  
- 
-data2<-data2%>%group_by(date, Echelle) %>% mutate(MeanNVSNGS = mean(slider_2.response)) 
-# 2 participants removed
+data2<-data2%>%group_by(date, Echelle) %>% mutate(MeanNVSNGS = mean(slider_2.response, na.rm=T)) 
+# 2 participants removed if method 1 (subset) is used
 # which(table(data2$date)==0)
 
 
@@ -119,42 +117,45 @@ for(i in 1:nrow(data)){ # Compute reverse scores
 
 ####################################
 ####################################
-data3<-subset(data, data$slider_3.rt > 1.5)
-# data3<-data
-# data3[] <- lapply(data3, function(x) ifelse(data3$slider_3.rt <1.5, NA, data3$slider_3.rt)) # To replace by NA instead of stupidly removing
-
+#data3<-subset(data, data$slider_3.rt > 1.5)
+data3 <-data
+data3[, "slider_3.rt"][data3[, "slider_3.rt"] <1.5] <- NA
 # which(table(data3$date)==0) #1 participant removed
 
 # By sub scale
 # Self-Kindness Items: 2, 6
 # Note: It removes participants answering in < 1.5 seconds on both items
-dataSelfKindness<-data3%>%group_by(date) %>%  filter(trials_4.thisIndex == "1" | trials_4.thisIndex== "5") %>% mutate(MeanSelfKind = mean(slider_3.response, na.rm = T))  %>% distinct(date)
+dataSelfKindness<-data3%>%group_by(date) %>%  filter(trials_4.thisIndex == "1" | trials_4.thisIndex== "5") %>% mutate(MeanSelfKind = mean(slider_3.response, na.rm = T)) # %>% distinct(date)
 
 # Self-Judgment Items: 11, 12
 # Note: It removes participants answering in < 1.5 seconds on both items
-dataSelfJudg<-data3%>%group_by(date) %>%  filter(trials_4.thisIndex == "10" | trials_4.thisIndex== "11") %>%mutate(MeanSelfJudg = mean(slider_3.response, na.rm = T)) %>% distinct(date)
+dataSelfJudg<-data3%>%group_by(date) %>%  filter(trials_4.thisIndex == "10" | trials_4.thisIndex== "11") %>%mutate(MeanSelfJudg = mean(slider_3.response, na.rm = T))# %>% distinct(date)
 
 # Common Humanity Items: 5, 10
 # Note: It removes participants answering in < 1.5 seconds on both items
-dataSelfJudg<-data3%>%group_by(date) %>%  filter(trials_4.thisIndex == "10" | trials_4.thisIndex== "11") %>%mutate(MeanSelfJudg = mean(slider_3.response, na.rm = T)) %>% distinct(date)
+dataCommHum<-data3%>%group_by(date) %>%  filter(trials_4.thisIndex == "4" | trials_4.thisIndex== "9") %>%mutate(MeanCommHum = mean(slider_3.response, na.rm = T))# %>% distinct(date)
 
 # Isolation Items: 4, 8
 # Note: It removes participants answering in < 1.5 seconds on both items
-dataSelfJudg<-data3%>%group_by(date) %>%  filter(trials_4.thisIndex == "10" | trials_4.thisIndex== "11") %>%mutate(MeanSelfJudg = mean(slider_3.response, na.rm = T)) %>% distinct(date)
+dataIsol<-data3%>%group_by(date) %>%  filter(trials_4.thisIndex == "3" | trials_4.thisIndex== "7") %>%mutate(MeanIsol = mean(slider_3.response, na.rm = T))# %>% distinct(date)
 
 # Mindfulness Items: 3, 7
 # Note: It removes participants answering in < 1.5 seconds on both items
-dataSelfJudg<-data3%>%group_by(date) %>%  filter(trials_4.thisIndex == "10" | trials_4.thisIndex== "11") %>%mutate(MeanSelfJudg = mean(slider_3.response, na.rm = T)) %>% distinct(date)
+dataMindf<-data3%>%group_by(date) %>%  filter(trials_4.thisIndex == "2" | trials_4.thisIndex== "6") %>%mutate(MeanMindf = mean(slider_3.response, na.rm = T)) #%>% distinct(date)
 
 # Over-identified Items: 1, 9
 # Note: It removes participants answering in < 1.5 seconds on both items
-dataSelfJudg<-data3%>%group_by(date) %>%  filter(trials_4.thisIndex == "10" | trials_4.thisIndex== "11") %>%mutate(MeanSelfJudg = mean(slider_3.response, na.rm = T)) %>% distinct(date)
+dataOverIden<-data3%>%group_by(date) %>%  filter(trials_4.thisIndex == "0" | trials_4.thisIndex== "8") %>%mutate(MeanOverIden = mean(slider_3.response, na.rm = T))# %>% distinct(date)
 
+data3<-data3%>%group_by(date) %>% mutate(MeanSCS = mean(slider_3.response, na.rm = T)) 
 
 
 
 #Compute GASP Scores
-data4<-subset(data, data$slider.rt > 0)
+#data4<-subset(data, data$slider.rt > 0)
+data4<-data
+data4[, "slider.rt"][data4[, "slider.rt"] <1.5] <- NA
+
 data4<-data4%>%group_by(date,EMOTION) %>% mutate(MeanGuiltShame = mean(slider.response, na.rm = T)) 
 
 # Compute average words RT
@@ -182,7 +183,7 @@ which(table(DataNVS$date)==0)
 
 which(table(DataWide$date)==0)
 
-DataWide<-DataWide[-which(DataWide$date=="2020-10-22_11h06.32.922"|DataWide$date=="2020-10-22_11h10.21.295"),]
+#DataWide<-DataWide[-which(DataWide$date=="2020-10-22_11h06.32.922"|DataWide$date=="2020-10-22_11h10.21.295"),]
 
 DataWide$Vnarc<-DataNVS$MeanNVSNGS  
 
@@ -190,7 +191,7 @@ DataNGS<-data2%>%filter(Echelle=="NGS")%>%distinct(MeanNVSNGS)
 which(table(DataNGS$date)==0)
 which(table(DataWide$date)==0)
 
-DataWide<-DataWide[-which(DataWide$date=="2020-10-22_10h48.24.644"|DataWide$date=="2020-10-22_11h02.12.941"|DataWide$date=="2020-10-22_11h03.12.421"),]
+#DataWide<-DataWide[-which(DataWide$date=="2020-10-22_10h48.24.644"|DataWide$date=="2020-10-22_11h02.12.941"|DataWide$date=="2020-10-22_11h03.12.421"),]
 
 
 DataWide$Gnarc<-DataNGS$MeanNVSNGS 
@@ -199,9 +200,9 @@ DataGuilt<-data4%>%filter(EMOTION == "GUILT")%>%distinct(MeanGuiltShame)
 DataShame<-data4%>%filter(EMOTION == "SHAME")%>%distinct(MeanGuiltShame)
 
 which(table(DataGuilt$date)==0)
-DataGuilt<-DataGuilt[-which(DataGuilt$date=="2020-10-22_10h48.24.644"|DataGuilt$date=="2020-10-22_11h02.12.941"|DataGuilt$date=="2020-10-22_11h03.12.421"|DataGuilt$date=="2020-10-22_11h06.32.922"|DataGuilt$date=="2020-10-22_11h10.21.295"),]
+#DataGuilt<-DataGuilt[-which(DataGuilt$date=="2020-10-22_10h48.24.644"|DataGuilt$date=="2020-10-22_11h02.12.941"|DataGuilt$date=="2020-10-22_11h03.12.421"|DataGuilt$date=="2020-10-22_11h06.32.922"|DataGuilt$date=="2020-10-22_11h10.21.295"),]
 which(table(DataShame$date)==0)
-DataShame<-DataShame[-which(DataShame$date=="2020-10-22_10h48.24.644"|DataShame$date=="2020-10-22_11h02.12.941"|DataShame$date=="2020-10-22_11h03.12.421"|DataShame$date=="2020-10-22_11h06.32.922"|DataShame$date=="2020-10-22_11h10.21.295"),]
+#DataShame<-DataShame[-which(DataShame$date=="2020-10-22_10h48.24.644"|DataShame$date=="2020-10-22_11h02.12.941"|DataShame$date=="2020-10-22_11h03.12.421"|DataShame$date=="2020-10-22_11h06.32.922"|DataShame$date=="2020-10-22_11h10.21.295"),]
 
 DataWide$ShameP<-DataShame$MeanGuiltShame
 DataWide$GuiltP<-DataGuilt$MeanGuiltShame
@@ -213,24 +214,40 @@ DataSC<-data3%>%distinct(MeanSCS)
 #DataSC<-as.data.frame(levels(as.factor(data3$MeanSCS)))
 which(table(DataSC$date)==0)
 which(table(DataWide$date)==0)
-DataSC<-DataSC[-which(DataSC$date=="2020-10-22_10h48.24.644"|DataSC$date=="2020-10-22_11h02.12.941"|DataSC$date=="2020-10-22_11h03.12.421"|DataSC$date=="2020-10-22_11h06.32.922"),]
+#DataSC<-DataSC[-which(DataSC$date=="2020-10-22_10h48.24.644"|DataSC$date=="2020-10-22_11h02.12.941"|DataSC$date=="2020-10-22_11h03.12.421"|DataSC$date=="2020-10-22_11h06.32.922"),]
 
 
 
 
 DataWide$SelfComp<-DataSC$MeanSCS
 
+dataSelfJudg<-dataSelfJudg%>%distinct(MeanSelfJudg)
+dataSelfKindness<-dataSelfKindness%>%distinct(MeanSelfKind)
+dataOverIden<-dataOverIden%>%distinct(MeanOverIden)
+dataIsol<-dataIsol%>%distinct(MeanIsol)
+dataCommHum<-dataCommHum%>%distinct(MeanCommHum)
+dataMindf<-dataMindf%>%distinct(MeanMindf)
+
+DataWide$MeanSelfJudg<-dataSelfJudg$MeanSelfJudg
+DataWide$MeanSelfKind<-dataSelfKindness$MeanSelfKind
+DataWide$MeanOveriden<-dataOverIden$MeanOverIden
+  DataWide$MeanIsol<-dataIsol$MeanIsol
+  DataWide$MeanCommhum<-dataCommHum$MeanCommHum
+  DataWide$MeanMindf<-dataMindf$MeanMindf
+
+
+
 
 #####Ces participants ont au moins UN questionnaire exclusivement associé à des TR < 1.5s      
-data<-data[-which(data$date=="2020-10-22_10h48.24.644"|
-                    data$date=="2020-10-22_10h48.58.053"|
-                    data$date=="2020-10-22_10h52.48.818"|
-                    data$date=="2020-10-22_10h55.45.886"|
-                    data$date=="2020-10-22_10h58.02.161"|
-                    data$date=="2020-10-22_11h02.12.941"|
-                    data$date=="2020-10-22_11h03.12.421"|
-                    data$date=="2020-10-22_11h06.32.922"|
-                    data$date=="2020-10-22_11h10.21.295"),]
+#data<-data[-which(data$date=="2020-10-22_10h48.24.644"|
+#                    data$date=="2020-10-22_10h48.58.053"|
+#                    data$date=="2020-10-22_10h52.48.818"|
+#                    data$date=="2020-10-22_10h55.45.886"|
+#                    data$date=="2020-10-22_10h58.02.161"|
+#                    data$date=="2020-10-22_11h02.12.941"|
+#                    data$date=="2020-10-22_11h03.12.421"|
+#                    data$date=="2020-10-22_11h06.32.922"|
+#                    data$date=="2020-10-22_11h10.21.295"),]
 dataPart<-data%>%filter(EMOTION =="GUILT")%>%distinct(participant)
 dataGenre<-data%>%filter(EMOTION =="GUILT")%>%distinct(Gender..Male.Female.Other..)
 dataAge<-data%>%filter(EMOTION =="GUILT")%>%distinct(Age.)
@@ -247,7 +264,7 @@ DataWide$CheckName<-DataName$key_resp_9.keys
 
 #ADD EXPERIMENTAL CONDITION : if participant number is even --> Experimental / If odd --> Control
 DataWide$Cond<- NA
-
+# A vérifier
 for(i in 1:nrow(DataWide)){
   if( (DataWide$participant[i]%%2) == 0) {DataWide$Cond[i]<-"Name"}
   else{DataWide$Cond[i]<-"control"}
@@ -286,17 +303,23 @@ DataWide$Genre<-recode(DataWide$Genre,
                        M = "MALE",
                        o = "OTHER",
                        O= "OTHER",
-                       Other="OTHER",  .default = levels(Set$Genre))
+                       Other="OTHER",  .default = levels(DataWide$Genre))
 
-write.csv(DataWide, "Data Prolific Study.csv")
-
-
+write.csv(DataWide, "ProliData.csv")
 
 
-Set<-Data.Prolific.Study.csv
+
+
+Set<-read.csv("ProliData.csv")
 library(MASS)
 library(reghelper)
 str(Set)
+
+Set<-Set %>%
+  mutate_if(is.numeric, scale)
+
+
+
 summary(lm(Set$FailureRT~Set$Cond + Set$GuiltP + Set$ShameP + Set$Gnarc + Set$Vnarc + Set$Cond:Set$GuiltP +Set$Cond:Set$ShameP + Set$Cond:Set$Gnarc + Set$Cond:Set$Vnarc + Set$NeutralRT))
 
 SetName<-subset(Set, Set$CheckName=="y")
@@ -318,28 +341,49 @@ gg <- ggplot(SetMadF, aes(x=Vnarc, y=ShameP)) +
        x="V Narc score")
 
 
-summary(lm(log(EscapeRT)~Cond*GuiltP+NeutralRT+Age, data=SetMad))
-summary(lm(log(FailureRT)~Cond*GuiltP+NeutralRT+Age, data=SetMadF))
-summary(lm(log(SuccessRT)~Cond*GuiltP+NeutralRT+Age, data=SetMadS))
+summary(lm((EscapeRT)~Cond*GuiltP+NeutralRT+Age, data=SetMad))
+summary(lm((FailureRT)~Cond*GuiltP+NeutralRT+Age, data=SetMadF))
+summary(lm((SuccessRT)~Cond*GuiltP+NeutralRT+Age, data=SetMadS))
 
-summary(lm(log(EscapeRT)~Cond*ShameP+NeutralRT+Age, data=SetMad))
-summary(lm(log(FailureRT)~Cond*ShameP+NeutralRT+Age, data=SetMadF))
-summary(lm(log(SuccessRT)~Cond*ShameP+NeutralRT+Age, data=SetMadS))
+summary(lm((EscapeRT)~Cond*ShameP+NeutralRT+Age, data=SetMad))
+summary(lm((FailureRT)~Cond*ShameP+NeutralRT+Age, data=SetMadF))
+summary(lm((SuccessRT)~Cond*ShameP+NeutralRT+Age, data=SetMadS))
 
-summary(lm(log(EscapeRT)~Cond*Vnarc+NeutralRT+Age, data=SetMad))
-summary(lm(log(FailureRT)~Cond*Vnarc+NeutralRT+Age, data=SetMadF))
-summary(lm(log(SuccessRT)~Cond*Vnarc+NeutralRT+Age, data=SetMadS))
+summary(lm((EscapeRT)~Cond*Vnarc+NeutralRT+Age, data=SetMad))
+summary(lm((FailureRT)~Cond*Vnarc+NeutralRT+Age, data=SetMadF))
+summary(lm((SuccessRT)~Cond*Vnarc+NeutralRT+Age, data=SetMadS))
 
-summary(lm(log(EscapeRT)~Cond*Gnarc+NeutralRT+Age, data=SetMad))
-summary(lm(log(FailureRT)~Cond*Gnarc+NeutralRT+Age, data=SetMadF))
-summary(lm(log(SuccessRT)~Cond*Gnarc+NeutralRT+Age, data=SetMadS))
+summary(lm((EscapeRT)~Cond*Gnarc+NeutralRT+Age, data=SetMad))
+summary(lm((FailureRT)~Cond*Gnarc+NeutralRT+Age, data=SetMadF))
+summary(lm((SuccessRT)~Cond*Gnarc+NeutralRT+Age, data=SetMadS))
 
-summary(lm(log(EscapeRT)~Cond*SelfComp+NeutralRT+Age, data=SetMad))
-summary(lm(log(FailureRT)~Cond*SelfComp+NeutralRT+Age, data=SetMadF))
-summary(lm(log(SuccessRT)~Cond*SelfComp+NeutralRT+Age, data=SetMadS))
+summary(lm((EscapeRT)~Cond*SelfComp+NeutralRT+Age, data=SetMad))
+summary(lm((FailureRT)~Cond*SelfComp+NeutralRT+Age, data=SetMadF))
+summary(lm((SuccessRT)~Cond*SelfComp+NeutralRT+Age, data=SetMadS))
 
+summary(lm((EscapeRT)~Cond*MeanCommhum+NeutralRT+Age, data=SetMad))
+summary(lm((FailureRT)~Cond*MeanCommhum+NeutralRT+Age, data=SetMadF))
+summary(lm((SuccessRT)~Cond*MeanCommhum+NeutralRT+Age, data=SetMadS))
 
+summary(lm((EscapeRT)~Cond*MeanMindf+NeutralRT+Age, data=SetMad))
+summary(lm((FailureRT)~Cond*MeanMindf+NeutralRT+Age, data=SetMadF))
+summary(lm((SuccessRT)~Cond*MeanMindf+NeutralRT+Age, data=SetMadS))
 
+summary(lm((EscapeRT)~Cond*MeanSelfKind+NeutralRT+Age, data=SetMad))
+summary(lm((FailureRT)~Cond*MeanSelfKind+NeutralRT+Age, data=SetMadF))
+summary(lm((SuccessRT)~Cond*MeanSelfKind+NeutralRT+Age, data=SetMadS))
+
+summary(lm((EscapeRT)~Cond*MeanIsol+NeutralRT+Age, data=SetMad))
+summary(lm((FailureRT)~Cond*MeanIsol+NeutralRT+Age, data=SetMadF))
+summary(lm((SuccessRT)~Cond*MeanIsol+NeutralRT+Age, data=SetMadS))
+
+summary(lm((EscapeRT)~Cond*MeanSelfJudg+NeutralRT+Age, data=SetMad))
+summary(lm((FailureRT)~Cond*MeanSelfJudg+NeutralRT+Age, data=SetMadF))
+summary(lm((SuccessRT)~Cond*MeanSelfJudg+NeutralRT+Age, data=SetMadS))
+
+summary(lm((EscapeRT)~Cond*MeanOveriden+NeutralRT+Age, data=SetMad))
+summary(lm((FailureRT)~Cond*MeanOveriden+NeutralRT+Age, data=SetMadF))
+summary(lm((SuccessRT)~Cond*MeanOveriden+NeutralRT+Age, data=SetMadS))
 
 summary(lm((EscapeRT)~Cond*GuiltP+NeutralRT+Age + ShameP, data=SetMad))
 summary(lm((FailureRT)~Cond*GuiltP+NeutralRT+Age+ ShameP, data=SetMadF))
@@ -361,3 +405,20 @@ summary(lm((EscapeRT)~Cond*SelfComp+NeutralRT+Age+ ShameP, data=SetMad))
 summary(lm((FailureRT)~Cond*SelfComp+NeutralRT+Age+ ShameP, data=SetMadF))
 summary(lm((SuccessRT)~Cond*SelfComp+NeutralRT+Age+ ShameP, data=SetMadS))
 
+
+
+
+summary(lm(Vnarc~ShameP+GuiltP, data=Set))
+summary(lm(Gnarc~ShameP+GuiltP, data=Set))
+
+summary(lm(Vnarc~ShameP+GuiltP+SelfComp, data=Set))
+summary(lm(Gnarc~ShameP+GuiltP+SelfComp, data=Set))
+
+summary(lm(Vnarc~MeanCommhum+MeanOveriden+MeanSelfJudg+MeanSelfKind+MeanIsol+MeanMindf, data=Set))
+summary(lm(Gnarc~MeanCommhum+MeanOveriden+MeanSelfJudg+MeanSelfKind+MeanIsol+MeanMindf, data=Set))
+
+gg <- ggplot(SetMadF, aes(x=Vnarc, y=FailureRT)) + 
+  geom_point(aes(col=Cond)) + 
+  geom_smooth(aes(col=Cond),method="lm", se=F) + 
+  labs(y="FailureRT", 
+       x="Vnarc")
